f7fc1160ab23c20584ae81fc6ba3862ff576b8c4
Dennis Rogozhnikov <d_rogo03@uni-muenster.de>
2016-11-12 03:36:55 +0100

    commiting final version (1.0)

--- init.bat
+++ init.bat
@@ -9,4 +9,10 @@ copy /-y src\stubs\after.sh "%homesteadRoot%\after.sh"
 copy /-y src\stubs\aliases "%homesteadRoot%\aliases"
 
 set homesteadRoot=
+
+SET NEWLINE=^& echo.
+
+FIND /C /I "swifer.dev" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO 192.168.10.10 swifer.dev>>%WINDIR%\System32\drivers\etc\hosts
+
 echo Homestead initialized!
